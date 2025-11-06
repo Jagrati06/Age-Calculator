fe05cd297c4ded9af0b6b91e9f97b722874e3a67
Jagrati06 <jagratijain313@gmail.com>
2025-11-06 17:28:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AgeCalculator from "./Components/AgeCalculator"
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <AgeCalculator/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/AgeCalculator.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./AgeCalculator.css";
+const AgeCalculator = () => {
+
+    let [dob, setDob] = useState("");
+    let [age, setAge] = useState(null);
+
+    const findAge = () => {
+        if (dob) {
+            let todayDate = new Date();
+            let birthDate = new Date(dob)
+
+            let years = todayDate.getFullYear() - birthDate.getFullYear();
+            let months = todayDate.getMonth() - birthDate.getMonth();
+            let days = birthDate.getDate() - todayDate.getDate();
+
+            setAge({ years, months, days });
+        }
+    };
+    return (
+        <div className="age-box">
+            <input type="date" value={dob} onChange={(e) => setDob(e.target.value)} />
+            <button id="btn" onClick={findAge}>Calculate Age</button>
+
+            {age && (
+                <p>
+                    Your Age is <span>{age.years}</span> years,
+                    <span> {age.months}</span> months,
+                    <span> {age.days}</span> days.
+                </p>
+            )}
+
+
+
+        </div>
+    )
+};
+
+export default AgeCalculator;
